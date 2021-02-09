538bf05b350f9df7dd0c96729c5f4f8bb428c9f3
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-02-09 23:56:06 +0000

    defines class relationships

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ class User(db.Model):
 
     def __repr__(self):
         """Show User Id and names"""
-        return f'<User user_id={self.user_id} fname={self.fname} lname={self.lname}>'
+        return f'<User user_id={self.user_id} name={self.fname} {self.lname}>'
 
 
 class Teacher(db.Model):
@@ -27,9 +27,11 @@ class Teacher(db.Model):
     teacher_id = db.Column(db.Integer, primary_key=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
 
+    user = db.relationship('User', backref='teacher', uselist=False)
+
     def __repr__(self):
         """Show Teacher ID/Corresponding User Id"""
-        return f'<Teacher teacher_id={self.teacher_id} user_id={self.user_id}>'
+        return f'<Teacher teacher_id={self.teacher_id} name={self.user.fname} {self.user.lname}>'
 
 
 class Student(db.Model):
@@ -39,25 +41,58 @@ class Student(db.Model):
     student_id = db.Column(db.Integer, primary_key=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.teacher_id'), nullable=False)
-    program_name = db.Columnt(db.String(50), nullable=True)
+    program_name = db.Column(db.String(50), nullable=True)
+
+    teacher = db.relationship('Teacher', backref='students')
+    user = db.relationship('User', backref='student', uselist=False)
+
 
     def __repr__(self):
         """Show Student ID"""
-        return f'<Student student_id={self.student_id} user_id={self.user_id}>'
+        return f'<Student student_id={self.student_id} name={self.user.fname} {self.user.lname}>'
 
 
 class Log(db.Model):
     """Data Model for Practice Logs"""
     __tablename__ = 'logs'
-
     
     log_id = db.Column(db.Integer, primary_key=True, nullable=False)
     student_id = db.Column(db.Integer, db.ForeignKey('students.student_id'), nullable=False)
     log_date = db.Column(db.Date, nullable=False)
-    start_time = db.Column(db.Timestamp, nullable=False)
-    end_time = db.Column(db.Timestamp, nullable=False)
+    start_time = db.Column(db.String, nullable=False)
+    end_time = db.Column(db.String, nullable=False)
     pieces_practiced = db.Column(db.String(150), nullable=False)
+
+    student = db.relationship('Student', backref='logs')
 
     def __repr__(self):
         """Show Log Info"""
         return f'<Log log_date={self.log_date} student_id={self.student_id}>'
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///VMS', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    # from server import app
+
+    #sets Flask instance
+    from flask import Flask
+    app = Flask(__name__)
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+
+    connect_to_db(app)
+    db.create_all()
+
