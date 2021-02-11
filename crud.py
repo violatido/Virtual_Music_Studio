5d92dbb6d5e9723739d42922fc171be56e9f87d1
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-02-11 19:57:29 +0000

    Creates crud functions for user actions

--- crud.py
+++ crud.py
@@ -6,8 +6,23 @@ from model import db, User, Teacher, Student, Log, connect_to_db
 # # Functions start here!
 # """CRUD operations."""
 
-def create_user():
-    """ Create """
+def create_user(email, password, fname, lname):
+    """ Create an return a new user."""
+
+    user = User(email=email, password=password, fname=fname, lname=lname)
+
+    db.session.add(user)
+    db.session.commt()
+
+    return user
+
+def get_user_by_id(user_id):
+    """ Get the user email using the primary key: user_id """
+    return User.query.get(user_id)
+
+def get_user_by_email(email):
+    """ Get the user by using the email """
+    return User.query.filter(User.email == email).first()
 
 
 
