0f4ccd8537b83f8314c2e60c80925782d25d7c0d
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-02-12 01:24:22 +0000

    Creates all app routes and jinja templates

--- server.py
+++ server.py
@@ -18,6 +18,26 @@ def create_homepage():
     return render_template('homepage.html')
 
 
+@app.route('/sign-up')
+def sign_up():
+    """Renders the VMS sign-up page"""
+    return render_template('sign-up.html')
+
+@app.route('/student-profile')
+def view_student_profile():
+    """Renders the VMS student-profile page"""
+    return render_template('student-profile.html')
+
+@app.route('/teacher-profile')
+def view_teacher_profile():
+    """Renders the VMS teacher-profile page"""
+    return render_template('teacher-profile.html')
+
+@app.route('/practice-log')
+def create_log():
+    """Renders the VMS practice-log page"""
+    return render_template('practice-log.html')
+
 if __name__ == '__main__':
     connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
