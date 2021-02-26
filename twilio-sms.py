cd858359786db82097eff9ee23fabb80069cc83b
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-02-26 19:29:59 +0000

    creates first Twilio conversation

---
+++ twilio-sms.py
@@ -0,0 +1,24 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ["AC7de31e0e824bcdab8af440bd1a54effb"]
+auth_token = os.environ['f2a48906ec65a8a08b7ea11d6753670f']
+client = Client(account_sid, auth_token)
+
+conversation = client.conversations \
+                     .conversations \
+                     .create(friendly_name='My First Conversation')
+
+print(conversation.sid)
+
+participant = client.conversations \
+  .conversations("CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") \
+    .participants \
+    .create(
+        messaging_binding_address='<+16106806107>',
+        messaging_binding_proxy_address='<+17067409285>'
+    )
