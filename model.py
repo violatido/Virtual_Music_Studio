02301302f4edbb2af9a8649c0dd99ea05004b6a1
Ilana Rose Mercer <ilana.rose.mercer@gmail.com>
2021-04-08 18:07:21 +0000

    changes nullability for note table attributes

--- model.py
+++ model.py
@@ -24,13 +24,14 @@ class Teacher(db.Model):
         """Show Teacher ID and full name"""
         return f'<Teacher teacher_id={self.teacher_id} teacher_name={self.teacher_fname} {self.teacher_lname}>'
 
+    # utilize abstraction in the teacher class to allow teachers to view student info
     def get_student_ids(self):
         """ Gives teacher access to their students's student IDs """
 
         student_ids_lst = [student.student_id for student in self.students]
         return set(student_ids_lst)
 
-############################################################################
+#################################################################################################################
 
 class Student(db.Model):
     """Data Model for Student users"""
@@ -53,7 +54,7 @@ class Student(db.Model):
         """Show Student ID and full name"""
         return f'<Student student_id={self.student_id} student_name = {self.student_fname} {self.student_lname}>'
 
-########################################################################
+#################################################################################################################
 
 class Log(db.Model):
     """Data Model for Practice Logs"""
@@ -70,7 +71,7 @@ class Log(db.Model):
         """Show Log date, ID, and ID of student who created the log"""
         return f'<Log log_date={self.log_date} student_id={self.student_id} log_id={self.log_id}'
 
-########################################################################
+#################################################################################################################
 
 class Note(db.Model): 
     """Data Model for Teacher Lesson Notes"""
@@ -79,17 +80,17 @@ class Note(db.Model):
     
     note_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.teacher_id'))
-    note_student_name = db.Column(db.String)
-    note_date = db.Column(db.Date)
-    note_time = db.Column(db.Time)
-    note_content = db.Column(db.String)
+    note_student_name = db.Column(db.String, nullable=False)
+    note_date = db.Column(db.Date, nullable=False)
+    note_time = db.Column(db.Time, nullable=False)
+    note_content = db.Column(db.String, nullable=False)
 
 
     def __repr__(self):
         """Show Teacher Note ID and date"""
         return f'<note note_id={self.note_id} note_date = {self.note_date}>'
 
-########################################################################
+#################################################################################################################
 
 def connect_to_db(flask_app, db_uri='postgresql:///VMS', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
